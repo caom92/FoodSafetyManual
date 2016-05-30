9e2def7d87fa78d1614fe0550e8230985382ecbb
carlos <caom92@live.com>
2016-05-30 14:28:42 -0700

    Ya se envian reportes de bugs por correo electronico

--- source/client/controllers/reportBug.js
+++ source/client/controllers/reportBug.js
@@ -38,8 +38,8 @@ function requiredSelectFieldValidate(id) {
 
 
 // This function checks if the especified non-required text area input's 
-// content is ofthe allowed length, returning true if this is the case or false 
-// otherwise
+// content is of the allowed length, returning true if this is the case or 
+// false otherwise
 // [in]    id: the HTML tag ID of the input field which we want to 
 //         validate
 // [out]   return: true if the field was valid or false otherwise
@@ -69,18 +69,70 @@ $(function() {
     // Initialize text area character counters
     $("textarea").characterCounter();
     
-    // hide the general error message when trying to submit an invalid form
-    $(".form-error").hide();
+    // Hide the progress bar
+    $(".progress").hide();
     
-    // change a previously invalid select field valid when the user finally
+    // change a previously invalid select field to valid when the user finally
     // selects a valid option
     requredSelectFieldChanged("#zone-selection");
     requredSelectFieldChanged("#procedure-selection");
     requredSelectFieldChanged("#browser-selection");
     requredSelectFieldChanged("#severity-selection");
     
+    // when the user uploads one or more screenshots, we must check the MIME 
+    // type to make sure that the uploaded files are only images and not any
+    // other kind of file; if any of the files are not an image, prevent upload
+    $("#screenshot-attachment").change(function() {
+        // retrieve the files uploaded
+        files = document.getElementById("screenshot-attachment").files;
+        
+        // iterate each file 
+        for (file of files) {
+            // create a file reader to be able to read binary data from them
+            fileReader = new FileReader();
+            
+            // set a callback function for checking the real mime type of the 
+            // uploaded file
+            fileReader.onloadend = function(e) {
+                // read the first 4 bytes of the file, usually these contain the
+                // file format's magic number
+                headerBytes = (new Uint8Array(e.target.result)).subarray(0, 4);
+                
+                // initialize a string where we can store the magic number in
+                // a readable format
+                mime = "";
+                
+                // store each byte read from the header in the string
+                for (word of headerBytes) {
+                    mime += word.toString(16);
+                }
+                
+                // now do the actual MIME checking
+                // we check in the following order: 
+                // image/png, image/gif, image/jpeg (3 versions), and image/bmp
+                if (mime != "89504e47" && mime != "47494638" 
+                    && mime != "ffd8ffe0" && mime != "ffd8ffe1" 
+                    && mime != "ffd8ffe2" && mime.substr(0, 4) != "424d") {
+                    // if the file is invalid, mark the field in the form
+                    $(".file-path").removeClass("valid");
+                    $(".file-path").addClass("invalid");
+                    Materialize.toast(
+                        "S&oacute;lo se permite adjuntar im&aacute;genes", 
+                        3500, "rounded");
+                }
+            }
+            
+            // use the file reader that we created to read the data of the
+            // file
+            fileReader.readAsArrayBuffer(file);
+        }
+    });
+    
     // validate the form before submitting
     $("#bug-report").submit(function(e) {
+        // prevent authomatic submission, we'll do it manually
+        e.preventDefault();
+        
         // check if the required select inputs have a value selected;
         // if any of those is empty, mark it on the form
         zoneIsValid = requiredSelectFieldValidate("#zone-selection");
@@ -93,17 +145,44 @@ $(function() {
         
         // check that all area inputs have the proper length
         summaryLengthIsValid = textAreaFieldValidate("#summary");
-        stepsIsValid = textAreaFieldValidate("#steps");
-        expectationIsValid = textAreaFieldValidate("#expectation");
-        realityIsValid = textAreaFieldValidate("#reality");
+        stepsLengthIsValid = textAreaFieldValidate("#steps");
+        expectationLengthIsValid = textAreaFieldValidate("#expectation");
+        realityLengthIsValid = textAreaFieldValidate("#reality");
         
-        // if there is at least one missing, prevent submition and display the
-        // error mesage
+        // check if the user is attaching valid image files
+        attachmentIsValid = !$(".file-path").hasClass("invalid"); 
+        
+        // if there is at least one invalid missing, prevent submition and 
+        // display the error mesage
         if (!zoneIsValid || !procedureIsValid || !browserIsValid 
-            || !severityIsValid || !summaryIsValid || !expectationIsValid
-            || !realityIsValid) {
-            e.preventDefault();
-            $(".form-error").show();
+            || !severityIsValid || !summaryIsValid || !summaryLengthIsValid 
+            || !stepsLengthIsValid || !expectationLengthIsValid
+            || !realityLengthIsValid || !attachmentIsValid) {
+            Materialize.toast("Por favor corrija los campos incorrectos", 
+                3500, "rounded");
+        } else {
+            // retrieve the data from the form
+            formData = new FormData($(this)[0]);
+            
+            // show the progress bar
+            $(".progress").show();
+            
+            // if all form fields are valid, send the data to the server
+            $.ajax({
+                type: "POST",
+                url: "/espresso/services/others/mail-bug-report",
+                data: formData,
+                success: function(result) {
+                    Materialize.toast(
+                        "El reporte ha sido enviado con &eacute;xito", 
+                        3500, "rounded"
+                    );
+                    console.log("server says: " + result);
+                    $(".progress").hide();
+                },
+                contentType: false,
+                processData: false
+            });
         }
     });
 });
---
+++ source/server/services/others/mailBugReport.php
@@ -0,0 +1,95 @@
+<?php
+
+// Import PHPMailer using the composer autoloader
+require_once realpath(
+    "./../../../../external/PHPMailer/vendor/autoload.php"
+);
+
+// Create the email body by pasting all the posted data into it
+$body = "Usuario: " . $_POST["user-name"] . "\n"
+    . "ID de empleado: " . $_POST["user-id"] . "\n"
+    . "Zona: " . $_POST["zone-selection"] . "\n"
+    . "Procedimiento: " . $_POST["procedure-selection"] . "\n"
+    . "Navegadores: ";
+    
+// paste browsers
+foreach ($_POST["browser-selection"] as $browser) {
+    $body .= $browser . " ";
+}
+
+// continue with the rest of the body
+$body .= "\n" . "Severidad: " . $_POST["severity-selection"] . "\n"
+    . "Resumen: " . $_POST["summary"] . "\n"
+    . "Pasos para reproducirlo: " . $_POST["steps"] . "\n"
+    . "Salida esperada: " . $_POST["expectation"] . "\n"
+    . "Salida obtenida: " . $_POST["reality"] . "\n";
+
+
+//Create a new PHPMailer instance
+$mail = new PHPMailerOAuth;
+
+//Tell PHPMailer to use SMTP
+$mail->isSMTP();
+
+//Set the hostname of the mail server
+$mail->Host = "smtp.gmail.com";
+
+//Set the SMTP port number 
+$mail->Port = 587;
+
+//Set the encryption system to use 
+$mail->SMTPSecure = "tls";
+
+//Whether to use SMTP authentication
+$mail->SMTPAuth = true;
+
+//Set AuthType
+$mail->AuthType = "XOAUTH2";
+
+//User Email to use for SMTP authentication
+// Use the same Email used in Google Developer Console
+$mail->oauthUserEmail = "caom92@gmail.com";
+
+//Obtained From Google Developer Console
+$mail->oauthClientId = 
+"400565202453-2816cv5dbclt3s8l2u5p0qq8f713orrf.apps.googleusercontent.com";
+
+//Obtained From Google Developer Console
+$mail->oauthClientSecret = "PJdHoakwXn2IQ4p0L52eu9NW";
+
+//Obtained By running get_oauth_token.php after setting up APP in Google 
+// Developer Console.
+$mail->oauthRefreshToken = "1/SQZQxNs4NhjJcAYN6JWHYvsKcWQL0XRsQAaHsfuH3iI";
+
+//Set who the message is to be sent from
+//For gmail, this generally needs to be the same as the user you logged in as
+$mail->setFrom("caom92@gmail.com", "Espresso mailing system");
+
+//Set who the message is to be sent to
+$mail->addAddress("caom92@live.com", "Carlos Oliva");
+
+//Set the subject line
+$mail->Subject = "Jacobs Farm - Del Cabo: Bug Report";
+
+//Use plain text message
+$mail->isHTML(false);
+
+//Replace the plain text body with one created manually
+$mail->Body = $body;
+
+// attach the image files
+$length = count($_FILES["screenshot-attachment"]["tmp_name"]);
+
+for ($i = 0; $i < $length; $i++) {
+    $mail->AddAttachment($_FILES["screenshot-attachment"]["tmp_name"][$i], 
+    $_FILES["screenshot-attachment"]["name"][$i]);
+}
+
+// send the file
+if ($mail->Send()) {
+    echo "success";
+} else {
+    echo $mail->ErrorInfo;
+}
+
+?>
