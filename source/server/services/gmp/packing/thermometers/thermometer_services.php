8352c8844918b5825e08653dcc7fe7a7a6c5cf6c
root <caom92@live.com>
2017-03-07 20:28:52 -0800

    fixes de puros typos k hacen k PHP shore

--- source/server/services/gmp/packing/thermometers/thermometer_services.php
+++ source/server/services/gmp/packing/thermometers/thermometer_services.php
@@ -6,6 +6,19 @@ require_once realpath(dirname(__FILE__).'/../../../globals.php');
 
 use fsm;
 
-$gmpPackingThermoServices = [];
+$gmpPackingThermoServices = [
+    'log-gmp-packing-thermo-calibration' => [
+        'requirements_desc' => [
+            'logged_in' => ['Manager', 'Supervisor', 'Employee'],
+            'has_privileges' => [
+                'privilege' => ['Read', 'Write'],
+                'program' => 'GMP',
+                'module' => 'Packing',
+                'log' => 'Daily Thermometer Calibration Verification Check'
+            ]
+        ], 
+        'callback' => 'fsm\service\gmp\packing\thermometers'
+    ]
+];
 
 ?>
