dab52422a0cf47397e9419d66b64f471c9477e29
root <caom92@live.com>
2017-03-09 15:11:04 -0800

    Agregue servicios para inventario de glass & plastic

--- source/server/dao/gmp/packing/glass/AreaGlassDAO.php
+++ source/server/dao/gmp/packing/glass/AreaGlassDAO.php
@@ -17,6 +17,53 @@ class AreaGlassDAO extends db\DataAccessObject
     {
         parent::__construct("gmp_packing_glass_area_glass");
     }
+
+
+    // Toggle the activation status of the area glass with the especified ID
+    function toggleActivationByID($itemID)
+    {
+        return parent::$dataBase->query(
+            "UPDATE $this->table
+            SET is_active = !is_active
+            WHERE id = '$itemID'"
+        )->fetchAll();
+    }
+
+
+    // Returns the number of items associated to the specified working area
+    function countByAreaID($areaID)
+    {
+        return parent::count(['area_id' => $areaID]);
+    }
+
+
+    // Insert the specified rows to the table
+    function insert($rows) 
+    {
+        return parent::insert($rows);
+    }
+
+
+    // Returns a list of all items that belong to the working area with the 
+    // especified ID
+    function selectByAreaID($areaID)
+    {
+        return parent::select(
+            [ 
+                "$this->table.id",
+                'position(order)',
+                "$this->table.name",
+                'quantity',
+                'is_active'
+            ],
+            [ 
+                'area_id' => $areaID,
+                'ORDER' => [
+                    'position'
+                ]
+            ]
+        );
+    }
 }
 
 ?>
