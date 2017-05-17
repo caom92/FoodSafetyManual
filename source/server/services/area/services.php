e4f2596ae06cd2c51eee969e6bba72f83fbed1b2
Carlos Oliva <caom92@live.com>
2017-05-17 12:52:28 -0700

    Arregle bugs de macotela

--- source/server/services/area/services.php
+++ source/server/services/area/services.php
@@ -3,13 +3,13 @@
 $area = [
   'tables' => [
     'WorkingAreas' =>
-      realpath(dirname(__FILE__).'/services/area/WorkingAreas.php')
+      realpath(dirname(__FILE__).'/WorkingAreas.php')
   ],
   'services' => [
     'add-workplace-area' =>
-      realpath(dirname(__FILE__).'/services/area/add-workplace-area.php'),
+      realpath(dirname(__FILE__).'/add-workplace-area.php'),
     'get-areas-of-zone' =>
-        realpath(dirname(__FILE__).'/services/area/get-areas-of-zone.php')
+      realpath(dirname(__FILE__).'/get-areas-of-zone.php')
   ]
 ];
 
