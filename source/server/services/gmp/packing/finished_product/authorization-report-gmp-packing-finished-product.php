b4fca32db0f82bd3de7e9aa0452ced4b8b6c675b
Carlos Oliva <caom92@live.com>
2017-05-27 21:40:06 -0700

    Modificaciones a autorization para unusual occurrence y finished product

--- source/server/services/gmp/packing/finished_product/authorization-report-gmp-packing-finished-product.php
+++ source/server/services/gmp/packing/finished_product/authorization-report-gmp-packing-finished-product.php
@@ -8,16 +8,46 @@ $service = fsm\createAuthorizationReportService(
   'Packing',
   'Daily Finished Product Check',
   [
-    'items_name' => 'entries',
+    'items_name' => 'items',
     'extra_info' => [
       // NULL
     ],
     'function' => function($scope, $segment, $logDate) {
       // get the list of all entries from the database
-      return $scope->daoFactory->get('gmp\packing\finishedProduct\Logs')
+      $entries = $scope->daoFactory->get('gmp\packing\finishedProduct\Logs')
         ->selectByCaptureDateID(
           $logDate['id']
         );
+
+        // and then, obtain the list of all production areas
+        $areas = $scope->daoFactory
+          ->get('gmp\packing\finishedProduct\ProductionAreas')->selectByZoneID(
+            $segment->get('zone_id')
+          );
+
+        // then, obtain the list of all suppliers
+        $suppliers = $scope->daoFactory->get('Suppliers')->selectCode();
+
+        // after that, obtain the list of products
+        $products = $scope->daoFactory->get('Products')->selectCode();
+
+        // then, obtain the list of customers
+        $customers = $scope->daoFactory->get('Customers')->selectName();
+
+        // and then, obtain the list of quality types
+        $qualityTypes = $scope->daoFactory->get('QualityTypes')->selectAll();
+
+        // finally, return the log info
+        return [
+          'log_info' => [
+            'production_areas' => $areas,
+            'suppliers' => $suppliers,
+            'product_codes' => $products,
+            'customers' => $customers,
+            'quality_types' => $qualityTypes
+          ],
+          'entries' => $entries
+        ];
     }
   ]
 );
