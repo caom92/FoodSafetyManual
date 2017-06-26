b3b6f817f2e6cd35cc2b3e21bd1425f1bb4ce3e0
Carlos Oliva <caom92@live.com>
2017-06-25 21:31:41 -0700

    Fix typo en add-zone

--- source/server/services/zone/add-zone.php
+++ source/server/services/zone/add-zone.php
@@ -6,14 +6,35 @@ $service = [
     'new_zone' => [
       'type' => 'string',
       'length' => 3
+    ],
+    'company_name' => [
+      'type' => 'string',
+      'min_length' => 1,
+      'max_length' => 255
+    ],
+    'company_address' => [
+      'type' => 'string',
+      'min_length' => 1,
+      'max_length' => 255
+    ],
+    'logo' => [
+      'type' => 'files',
+      'format' => 'bitmap'
     ]
   ],
   'callback' => function($scope, $request) {
     $zones = $scope->daoFactory->get('Zones'); 
     $isZoneNameDuplicated = $zones->hasByName($request['new_zone']);
 
+
+
     if (!$isZoneNameDuplicated) {
-      $zones->insert($request['new_zone']);
+      $zones->insert([
+        'name' => $request['new_zone'],
+        'company_name' => $request['company_name'],
+        'address' => $request['company_address'],
+        'logo_path' => $request
+      ]);
       return [];
     } else {
       throw new \Exception('Cannot add new zone; name is already taken.');
