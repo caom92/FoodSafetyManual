623c9002055ae0c2d2b7bfc4ea82ee398ca29c4a
Carlos Oliva <caom92@live.com>
2017-06-27 01:53:10 -0700

    Cambie capture de atp para que reciva cadena en  lugar de ID

--- source/server/services/footers/add-report-footer.php
+++ source/server/services/footers/add-report-footer.php
@@ -13,11 +13,11 @@ $service = [
     ],
     'capture_form_footer' => [
       'type' => 'string',
-      'max_length' => 65538
+      'max_length' => 65535
     ],
     'report_document_footer' => [
       'type' => 'string',
-      'max_length' => 65538
+      'max_length' => 65535
     ]
   ],
   'callback' => function($scope, $request) {
