b170ec2964bff79be859496ec115ddff11e2c9c0
Carlos Oliva <caom92@live.com>
2017-06-26 17:55:56 -0700

    Fix error en edit de los pies de pagina

--- source/server/services/footers/services.php
+++ source/server/services/footers/services.php
@@ -7,7 +7,7 @@ $footers = [
   'services' => [
     'add-report-footer' => realpath(dirname(__FILE__).'/add-report-footer.php'),
     'edit-report-footers' => 
-      realpath(dirname(__FILE__).'/edit-report-footer.php'),
+      realpath(dirname(__FILE__).'/edit-report-footers.php'),
     'list-report-footers' => 
       realpath(dirname(__FILE__).'/list-report-footers.php'),
     'get-report-footer' => realpath(dirname(__FILE__).'get-report-footer.php')
