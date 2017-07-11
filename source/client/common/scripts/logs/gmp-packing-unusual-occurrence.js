5ad103153c5c77fbaa3646f06d5b6a94974182a8
Víctor Miracle Macotela <victor.miracle@uabc.edu.mx>
2017-07-11 10:50:01 -0700

    Fix en URL de NUOCA

--- source/client/common/scripts/logs/gmp-packing-unusual-occurrence.js
+++ source/client/common/scripts/logs/gmp-packing-unusual-occurrence.js
@@ -567,7 +567,7 @@ function gmpPackingUnusualOccurrenceReportAction(item){
 }
 
 function gmpPackingUnusualOccurrenceReportAlbumURL(item){
-    return {"type":"td","classes":"fullColumn","colspan":3,"contents":"<span class='url_title'></span>: <a href='" + item.entry[0].albumURL + "' >" + item.entry[0].albumURL + "</a>"};
+    return {"type":"td","classes":"fullColumn","colspan":3,"contents":"<span class='url_title'></span>: <a href='" + item.entry[0].album_url + "' >" + item.entry[0].album_url + "</a>"};
 }
 
 function getCSS(){
