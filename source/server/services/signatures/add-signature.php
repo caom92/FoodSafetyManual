1c7936b5216475d2ae0d6f844e9300a437846e2e
Carlos Oliva <caom92@live.com>
2017-07-12 14:12:33 -0700

    Agregue servicio para agregar firmas de supervisor

---
+++ source/server/services/signatures/add-signature.php
@@ -0,0 +1,45 @@
+<?php
+
+$service = [
+  'requirements_desc' => [
+    'logged_in' => ['Administrator'],
+    'supervisor_id' => [
+      'type' => 'int',
+      'min' => 1
+    ],
+    'signature_file' => [
+      'type' => 'files',
+      'format' => 'bitmap'
+    ]
+  ],
+  'callback' => function($scope, $request) {
+    // get the current date
+    $fileName = "{$request['supervisor_id']}" . date('Y-m-d_H-i-s');
+
+    // if it was, compute the full directory path where the file
+    // will be stored
+    $uploadDir = realpath(
+      dirname(__FILE__)."/../../../data/signatures") . "/{$fileName}";
+
+    // finally save the uploaded file as the current manual file
+    $wasMoveSuccessful = move_uploaded_file(
+      $_FILES['signature_file']['tmp_name'], 
+      $uploadDir
+    );
+
+    // and check if the file was saved successfully
+    if (!$wasMoveSuccessful) {
+      // and notify the user of the error
+      throw new \Exception(
+        'The file '.$_FILES['signature_file']['name'].
+        ' could not be uploaded.'
+      );
+    }
+
+    // store the file path in the database
+    $scope->daoFactory->get('Users')->updateSignaturePathByID(
+      $request['supervisor_id'], $fileName);
+  }
+];
+
+?>
