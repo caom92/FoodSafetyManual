c938698446da9006330cd4e0983d473d974523fe
Carlos Oliva <caom92@live.com>
2017-07-19 15:16:40 -0700

    Fix bug en nomenclatura de archivos subidos en capture de doc control

--- source/server/services/gmp/doc_control/doc_control/capture-gmp-doc-control-doc-control.php
+++ source/server/services/gmp/doc_control/doc_control/capture-gmp-doc-control-doc-control.php
@@ -105,7 +105,10 @@ $service = fsm\createCaptureService(
                 substr($originalFileName, strpos($originalFileName, '.'));
 
               // generamos el nombre del archivo
-              $fileName = "{$logID}_" . date('Y-m-d_H-i-s') . "_$k$format";
+              $fileName = 
+                "{$logID}_" . 
+                date('Y-m-d_H-i-s') . 
+                "_{$i}_{$j}_$k$format";
 
               $s = NULL;
               if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
