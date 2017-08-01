5813ca32b73ee87ac0d96d9ecf11bc55cd166e3c
Carlos Oliva <caom92@live.com>
2017-08-01 14:19:28 -0700

    Log de Doc Control ahora puede ser invocado por el Director

--- source/server/services/gmp/doc_control/doc_control/log-gmp-doc-control-doc-control.php
+++ source/server/services/gmp/doc_control/doc_control/log-gmp-doc-control-doc-control.php
@@ -3,18 +3,44 @@
 require_once realpath(dirname(__FILE__).'/../../../service_creators.php');
 
 
-$service = fsm\createLogService(
-  'GMP',
-  'Document Control',
-  'Document Control',
-  [
-    'items_name' => 'documents',
-    'function' => function($scope, $segment) {
+$service = [
+  'requirements_desc' => [
+    'logged_in' => ['Manager', 'Supervisor', 'Employee', 'Director'],
+    'has_privileges' => [
+      'privilege' => ['Read', 'Write'],
+      'program' => 'GMP',
+      'module' => 'Document Control',
+      'log' => 'Document Control'
+    ],
+    'callback' => function($scope, $request) {
+      // first, get the session segment
       $segment = $scope->session->getSegment('fsm');
-      return $scope->daoFactory->get('gmp\docControl\docControl\Documents')
+
+      // retrieve the list of log items from the database
+      $segment = $scope->session->getSegment('fsm');
+      $items = $scope->daoFactory->get('gmp\docControl\docControl\Documents')
         ->selectActiveByZoneID($segment->get('zone_id'));
+
+      // retrieve the footers for the capture form
+      $footers = $scope->daoFactory->get('ReportFooters')
+        ->getByZoneIDAndLogID(
+          $segment->get('zone_id'),
+          $scope->daoFactory->get('Logs')->getIDByNames(
+            'GMP', 'Document Control', 'Document Control'
+          )
+        );
+
+      // prepare the response JSON
+      return [
+        'zone_name' => $segment->get('zone_name'),
+        'program_name' => $program,
+        'module_name' => $module,
+        'log_name' => $log,
+        'html_footer' => $footers['form_footer'],
+        'documents' => $items
+      ];
     }
   ]
-);
+];
 
 ?>
