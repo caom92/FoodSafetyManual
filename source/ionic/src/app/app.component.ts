bfa52e16dd14c6ceaecd136d3df53e08d45434f0
Víctor Miracle Macotela <victor.miracle@hotmail.com>
2017-09-13 16:50:02 -0700

    Ya se despliegan programas/modulos/bitácoras de acuerdo a los permisos del usuario. Se añadieron íconos y Splash Screen para Android. Se corrigieron múltiples bugs. Idioma ya funciona correctamente en las vistas y en el sidemenu.

--- source/ionic/src/app/app.component.ts
+++ source/ionic/src/app/app.component.ts
@@ -1,29 +1,44 @@
-import { Component, ViewChild } from '@angular/core';
-import { Nav, Platform, MenuController } from 'ionic-angular';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { NavController, Nav, Platform, MenuController, Events } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { HttpModule } from '@angular/http';
 import { Storage } from '@ionic/storage';
 
+import { Language } from 'angular-l10n';
+
 import { HomePage } from '../pages/home/home';
 import { BackendService } from '../services/app.backend';
+import { TranslationService } from '../services/app.translation';
 import { EditProfile } from '../pages/edit-profile/edit-profile';
+
+import { ModulesPage } from '../pages/modules/modules';
+import { LogsPage } from '../pages/logs/logs';
 
 @Component({
   templateUrl: 'app.html',
   providers: [
-    BackendService
+    BackendService,
+    TranslationService
   ]
 })
-export class MyApp {
+export class MyApp implements AfterViewInit{
   @ViewChild(Nav) nav: Nav;
+  @Language() lang: string;
 
   rootPage:any = HomePage;
 
-  pages: Array<{title: string, component: any, icon: string}>;
-  adminPages: Array<{title: string, component: any, icon: string}>;
+  menuLangEn: boolean = this.lang == "en"
+  menuLangEs: boolean = this.lang == "es"
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private storage: Storage, public menuCtrl: MenuController, private server: BackendService) {
+  pages_en: Array<{title: string, component: any, icon: string}>
+  pages_es: Array<{title: string, component: any, icon: string}>
+  adminPages_en: Array<{title: string, component: any, icon: string}>
+  adminPages_es: Array<{title: string, component: any, icon: string}>
+  programPages: Array<{title: string, component: any, icon: string}>
+  isAdminFlag: boolean = false
+
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private storage: Storage, public menuCtrl: MenuController, private server: BackendService, public events: Events, private translationService: TranslationService) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -31,17 +46,74 @@ export class MyApp {
       splashScreen.hide();
     });
 
-    this.pages = [
+    events.subscribe("user:loggedIn", (time, lang) => {
+      this.assignAdminFlag()
+      this.updatePermissions()
+      this.lang = lang
+      this.menuLangEn = this.lang == "en"
+      this.menuLangEs = this.lang == "es"
+    })
+
+    events.subscribe("language:changed", (lang, time) => {
+      this.lang = lang
+      this.menuLangEn = this.lang == "en"
+      this.menuLangEs = this.lang == "es"
+    })
+
+    this.pages_en = [
+      { title: 'Edit Profile', component: EditProfile, icon: "contact" }
+    ];
+
+    this.pages_es = [
       { title: 'Editar Perfil', component: EditProfile, icon: "contact" }
     ];
 
-    this.adminPages = [
+    this.programPages = []
+
+    this.programPages = [
+      /*{ title: 'GMP', component: EditProfile, icon: "build" },
+      { title: 'GAP', component: EditProfile, icon: "build" }*/
+    ];
+
+    this.adminPages_en = [
+      { title: 'Users', component: HomePage, icon: "people" },
+      { title: 'Zones', component: HomePage, icon: "map" },
+      { title: 'Programs', component: HomePage, icon: "copy" },
+      { title: 'Supervisors', component: HomePage, icon: "medal" },
+      { title: 'Signatures', component: HomePage, icon: "create" }
+    ];
+
+    this.adminPages_es = [
       { title: 'Usuarios', component: HomePage, icon: "people" },
       { title: 'Zonas', component: HomePage, icon: "map" },
       { title: 'Programas', component: HomePage, icon: "copy" },
       { title: 'Supervisores', component: HomePage, icon: "medal" },
       { title: 'Firmas', component: HomePage, icon: "create" }
     ];
+  }
+
+  updatePermissions(){
+    this.programPages = []
+    this.storage.get("privileges").then(
+      data => {
+        console.log("Privilegios del usuario conectado")
+        console.log(JSON.parse(data))
+        data = JSON.parse(data)
+        console.log(data.zones[0])
+        console.log("Programas")
+        for(var program of data.zones[0].programs){
+          this.programPages.push({title: program.name, component: EditProfile, icon:"build"})
+        }
+      }
+    )
+  }
+
+  ionViewDidLoad() {
+    this.updatePermissions()
+  }
+
+  ngAfterViewInit(){
+
   }
 
   openPage(page) {
@@ -54,6 +126,14 @@ export class MyApp {
     /*}*/
   }
 
+  openModules(event, program){
+    console.log("openModules")
+    console.log(program)
+    this.nav.push(ModulesPage, {
+      program: program
+    })
+  }
+
   closeSession(){
     this.server.update(
       'logout', 
@@ -61,14 +141,13 @@ export class MyApp {
       (response: Response) => {
         let result = JSON.parse(response['_body'].toString())
         if (result.meta.return_code == 0) {
-          // si la sesion fue cerrada correctamente, desactivamos la bandera y 
+          // si la sesion fue cerrada correctamente
           // redireccionamos al usuario a la pantalla de inicio de sesion
-          //localStorage.is_logged_in = false
-          //this.home.hideZoneMenu()
-          //this.router.go('login')
+          // y limpiamos el contenido del Storage
           this.storage.clear()
           this.nav.setRoot(HomePage)
-          this.menuCtrl.enable(false)
+          this.menuCtrl.enable(false, "es")
+          this.menuCtrl.enable(false, "en")
         } else {
           // si hubo un problema con la comunicacion con el servidor, 
           // desplegamos un mensaje de error al usuario
@@ -79,7 +158,35 @@ export class MyApp {
   }
 
   isAdmin(){
-    return localStorage["__mydb/_ionickv/role_name"] == '"Administrator"';
+    /*this.storage.get("role_name").then(
+      data => {
+        //this.isAdminFlag = (data == "Administrator")
+      }
+    )*/
+  }
+
+  assignAdminFlag(){
+    this.storage.get("role_name").then(
+      data => {
+        if(data == "Administrator")
+          this.isAdminFlag = true
+        else
+          this.isAdminFlag = false
+        console.log(this.isAdminFlag)
+      }
+    )
+  }
+
+  isEnglish(){
+    this.menuLangEn = true
+    this.menuLangEs = false
+    return this.lang == "en"
+  }
+
+  isSpanish(){
+    this.menuLangEn = false
+    this.menuLangEs = true
+    return this.lang == "es"
   }
 }
 
