80b3f1f420d7c091bee45f6d8a9b434b30b2f214
Víctor Miracle Macotela <victor.miracle@hotmail.com>
2017-10-12 15:46:16 -0700

    Servicio TimeService

---
+++ source/client2/dev/services/app.time.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core'
+
+// Servicio que 
+
+@Injectable()
+export class DateTimeService {
+  constructor() {
+  }
+
+  getISODate(date) {
+    var ISODate = "";
+
+    ISODate += date.getFullYear() + "-";
+
+    if (date.getMonth() < 9) {
+      ISODate += "0" + (date.getMonth() + 1) + "-";
+    } else {
+      ISODate += (date.getMonth() + 1) + "-";
+    }
+
+    if (date.getDate() < 10) {
+      ISODate += "0" + date.getDate();
+    } else {
+      ISODate += date.getDate();
+    }
+
+    return ISODate;
+  }
+
+  getISOTime(date) {
+    let ISOTime = "";
+
+    if (date.getHours() < 10) {
+      ISOTime += "0" + date.getHours() + ":";
+    } else {
+      ISOTime += date.getHours() + ":";
+    }
+
+    if (date.getMinutes() < 10) {
+      ISOTime += "0" + date.getMinutes();// + ":";
+    } else {
+      ISOTime += date.getMinutes();// + ":";
+    }
+
+    return ISOTime;
+  }
+}
