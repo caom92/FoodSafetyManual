9c09a9d671e788f6f3837e1660f40fc0553bc2ee
Víctor Miracle Macotela <victor.miracle@hotmail.com>
2017-12-19 11:40:05 -0800

    Interfaces de Autorización generalizadas

--- source/ionic/src/pages/logs/gmp-packing-hand-washing/interfaces/gmp.packing.hand.washing.authorization.interface.ts
+++ source/ionic/src/pages/logs/gmp-packing-hand-washing/interfaces/gmp.packing.hand.washing.authorization.interface.ts
@@ -1,22 +1,12 @@
-export interface Authorization {
-  report_id: string,
-  created_by: string,
-  approved_by: string,
-  creation_date: string,
-  approval_date: string,
-  zone_name: string,
-  program_name: string,
-  module_name: string,
-  log_name: string,
-  notes: string,
+import { SuperAuthorization } from "../../super-logs/super.logs.authorization.interface"
+
+export interface Authorization extends SuperAuthorization {
+  notes: string
   items: Array<AuthorizationItem>
 }
 
 export interface AuthorizationItem {
-  id: string,
-  0: string,
-  name: string,
-  1: string,
-  is_acceptable: string,
-  2: string
+  id: string
+  name: string
+  is_acceptable: string
 }
--- source/ionic/src/pages/logs/gmp-packing-scale-calibration/interfaces/gmp.packing.scale.calibration.authorization.interface.ts
+++ source/ionic/src/pages/logs/gmp-packing-scale-calibration/interfaces/gmp.packing.scale.calibration.authorization.interface.ts
@@ -1,13 +1,6 @@
-export interface Authorization {
-  report_id: string
-  created_by: string
-  approved_by: string
-  creation_date: string
-  approval_date: string
-  zone_name: string
-  program_name: string
-  module_name: string
-  log_name: string
+import { SuperAuthorization } from "../../super-logs/super.logs.authorization.interface"
+
+export interface Authorization extends SuperAuthorization {
   notes: string
   corrective_action: string
   types: {
