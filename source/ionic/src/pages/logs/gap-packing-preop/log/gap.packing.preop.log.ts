451c709bd4d11d7c9281f970ccff3b6ae2deef44
Víctor Miracle Macotela <victor.miracle@hotmail.com>
2017-12-26 10:53:40 -0800

    Se retiraron algunos imports no utilizados

--- source/ionic/src/pages/logs/gap-packing-preop/log/gap.packing.preop.log.ts
+++ source/ionic/src/pages/logs/gap-packing-preop/log/gap.packing.preop.log.ts
@@ -1,4 +1,4 @@
-import { Component, Input, NgModule, OnInit } from '@angular/core'
+import { Component, Input, OnInit } from '@angular/core'
 import { NavParams } from 'ionic-angular'
 import { DatePipe } from '@angular/common'
 
--- source/ionic/src/pages/logs/gap-packing-preop/type/gap.packing.preop.type.ts
+++ source/ionic/src/pages/logs/gap-packing-preop/type/gap.packing.preop.type.ts
@@ -1,4 +1,4 @@
-import { Component, Input, NgModule } from '@angular/core'
+import { Component, Input } from '@angular/core'
 import { Language } from 'angular-l10n'
 import { FormGroup } from '@angular/forms'
 import { LogType, CorrectiveAction } from '../interfaces/gap.packing.preop.log.interface'
