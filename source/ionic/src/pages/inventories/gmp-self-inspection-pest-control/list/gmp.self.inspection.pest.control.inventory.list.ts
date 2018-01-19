2f2642bb800f8ef9a8cfe1d6ddb2f519fc655415
Víctor Miracle Macotela <victor.miracle@hotmail.com>
2018-01-18 17:22:03 -0800

    -Comenzando el traslado de inventarios al cliente Web

--- source/ionic/src/pages/inventories/gmp-self-inspection-pest-control/list/gmp.self.inspection.pest.control.inventory.list.ts
+++ source/ionic/src/pages/inventories/gmp-self-inspection-pest-control/list/gmp.self.inspection.pest.control.inventory.list.ts
@@ -1,12 +1,11 @@
-import { Component, Input, OnInit, OnDestroy } from '@angular/core'
-import { Events } from 'ionic-angular'
-import { ISubscription } from 'rxjs/Subscription'
+import { Component, Input, OnChanges, OnDestroy, OnInit } from '@angular/core'
 import { Language } from 'angular-l10n'
-import { InventoryItem } from '../interfaces/gmp.self.inspection.pest.control.inventory.interface'
+import { Events } from 'ionic-angular'
 import { DragulaService } from 'ng2-dragula'
+
 import { InventoryService } from '../../../../services/app.inventory'
-import { SuperInventoryListComponent } from '../../super-inventory/super.inventory.list';
-import { OnChanges } from '@angular/core/src/metadata/lifecycle_hooks';
+import { SuperInventoryListComponent } from '../../super-inventory/super.inventory.list'
+import { InventoryItem } from '../interfaces/gmp.self.inspection.pest.control.inventory.interface'
 
 @Component({
   selector: 'gmp-self-inspection-pest-control-inventory-list',
@@ -19,7 +18,6 @@ import { OnChanges } from '@angular/core/src/metadata/lifecycle_hooks';
 export class GMPSelfInspectionPestControlInventoryListComponent extends SuperInventoryListComponent implements OnInit, OnDestroy, OnChanges {
   @Language() private lang: string
   @Input() items: Array<InventoryItem> = null
-  @Input() private printHeader: boolean = false
 
   constructor(dragulaService: DragulaService,
     events: Events,
@@ -27,28 +25,12 @@ export class GMPSelfInspectionPestControlInventoryListComponent extends SuperInv
     super(dragulaService, events, inventoryService)
   }
 
-  /**
-   * Informa a la clase padre del nombre que se le asignará a la bolsa de
-   * Dragula, el cual debe ser único. Con este nombre asignado, la
-   * inicialización del componente padre se encarga de inicializar las funciones
-   * de Dragula
-   * 
-   * @memberof GMPSelfInspectionPestControlInventoryListComponent
-   */
-
   public ngOnInit(): void {
     this.setBagName("gmp-self-inspection-pest-control-bag")
     this.setSuffix("gmp-self-inspection-pest-control")
     this.setInventory(this.items)
     super.ngOnInit()
   }
-
-  /**
-   * En caso de algún cambio en el modelo, se debe actualizar el inventario que
-   * es utilizado para el reordenamiento de esta modificación
-   * 
-   * @memberof GMPSelfInspectionPestControlInventoryListComponent
-   */
 
   public ngOnChanges(): void{
     this.setInventory(this.items)
