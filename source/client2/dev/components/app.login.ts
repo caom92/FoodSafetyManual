8d7d3a412b0229b539e2155a474cb39daafa0443
Carlos Oliva <caom92@live.com>
2018-02-21 10:18:34 -0800

    Fix bug donde desaparecía el menu lateral cuando el usuario entraba a login

--- source/client2/dev/components/app.login.ts
+++ source/client2/dev/components/app.login.ts
@@ -88,6 +88,7 @@ export class LogInComponent implements OnInit
             // si el usuario ya inicio sesion hay que redireccionar al usuario 
             // a la pagina principal
             localStorage.is_logged_in = true
+            this.home.displaySideNav()
             this.router.go('edit-profile')
           } else {
             // si el usuario no hay iniciado sesion, permitimos al usuario 
