65c6e038740b63059f7c6528cc0f383df410850f
Víctor Miracle Macotela <victor.miracle@hotmail.com>
2018-03-15 17:35:19 -0800

    Actualización de las validaciones de Glass Brittle

--- source/client2/dev/components/logs/gmp-packing-glass-brittle/log/gmp.packing.glass.brittle.log.ts
+++ source/client2/dev/components/logs/gmp-packing-glass-brittle/log/gmp.packing.glass.brittle.log.ts
@@ -1,7 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core'
-import { FormArray, FormBuilder, Validators } from '@angular/forms'
+import { FormArray, FormBuilder, Validators, FormGroup } from '@angular/forms'
 import { Language } from 'angular-l10n'
 
+import { LanguageService } from '../../../../services/app.language'
 import { LogService } from '../../../../services/app.logs'
 import { DateTimeService } from '../../../../services/app.time'
 import { ToastsService } from '../../../../services/app.toasts'
@@ -9,7 +10,7 @@ import { TranslationService } from '../../../../services/app.translation'
 import { SuperLogComponent } from '../../super-logs/super.logs.log'
 import { CaptureArea, CaptureItem } from '../interfaces/gmp.packing.glass.brittle.capture.interface'
 import { Log } from '../interfaces/gmp.packing.glass.brittle.log.interface'
-import { LanguageService } from '../../../../services/app.language';
+import { CustomValidators } from '../../../../directives/custom.validators';
 
 @Component({
   selector: 'gmp-packing-glass-brittle-log',
@@ -20,6 +21,10 @@ export class GMPPackingGlassBrittleLogComponent extends SuperLogComponent implem
   @Input() log: Log = { zone_name: null, program_name: null, module_name: null, log_name: null, html_footer: null, areas: [{ id: null, name: null, items: [{ id: null, name: null, order: null, quantity: null }] }] }
   @Language() lang: string
 
+  readonly maxLengths = {
+    notes: 65535
+  }
+
   constructor(private _fb: FormBuilder,
     private timeService: DateTimeService,
     private translationService: TranslationService,
@@ -29,33 +34,31 @@ export class GMPPackingGlassBrittleLogComponent extends SuperLogComponent implem
     super(logService, toasts)
   }
 
-  ngOnInit() {
+  public ngOnInit(): void {
     this.setSuffix("gmp-packing-glass-brittle")
     super.ngOnInit()
     this.initForm()
   }
 
-  initForm() {
+  public initForm(): void {
     this.captureForm = this._fb.group({
-      date: [this.timeService.getISODate(new Date()), [Validators.required, Validators.minLength(1)]],
-      time: [this.timeService.getISOTime(new Date()), [Validators.required, Validators.minLength(1)]],
-      notes: ['', [Validators.required, Validators.minLength(1)]],
+      date: [this.timeService.getISODate(new Date()), [Validators.required, CustomValidators.dateValidator()]],
+      time: [this.timeService.getISOTime(new Date()), [Validators.required, CustomValidators.timeValidator()]],
+      notes: ['', [Validators.required, Validators.maxLength(this.maxLengths.notes)]],
       areas: this._fb.array([])
     })
     const control = <FormArray>this.captureForm.controls['areas']
     let currentTime = this.timeService.getISOTime(new Date())
     for (let area of this.log.areas) {
-      console.log("area of log")
       let itemControl = []
       for (let item of area.items) {
-        console.log("item of area")
         itemControl.push(this.initItem({ id: item.id, is_acceptable: null }))
       }
       control.push(this.initArea({ id: area.id, items: itemControl }))
     }
   }
 
-  resetForm() {
+  public resetForm(): void {
     let areas = []
     let currentTime = this.timeService.getISOTime(new Date())
     for (let area of this.log.areas) {
@@ -73,14 +76,14 @@ export class GMPPackingGlassBrittleLogComponent extends SuperLogComponent implem
     })
   }
 
-  initArea(area: CaptureArea) {
+  public initArea(area: CaptureArea): FormGroup {
     return this._fb.group({
       id: [area.id, [Validators.required]],
       items: this._fb.array(area.items)
     })
   }
 
-  initItem(item: CaptureItem) {
+  public initItem(item: CaptureItem): FormGroup {
     return this._fb.group({
       id: [item.id, [Validators.required]],
       is_acceptable: [item.is_acceptable, [Validators.required]]
