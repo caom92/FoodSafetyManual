8c279782376df69bcf9ce05a4eac6ecce32cab3e
Víctor Miracle Macotela <victor.miracle@hotmail.com>
2018-03-15 17:22:47 -0800

    Actualización de validaciones de Hand Washing

--- source/client2/dev/components/logs/gmp-packing-hand-washing/log/gmp.packing.hand.washing.log.ts
+++ source/client2/dev/components/logs/gmp-packing-hand-washing/log/gmp.packing.hand.washing.log.ts
@@ -1,17 +1,15 @@
 import { Component, Input, OnInit } from '@angular/core'
-import { DatePipe } from '@angular/common'
-import { Validators, FormGroup, FormArray, FormBuilder } from '@angular/forms'
-
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms'
 import { Language } from 'angular-l10n'
 
+import { CustomValidators } from '../../../../directives/custom.validators'
+import { LogService } from '../../../../services/app.logs'
+import { DateTimeService } from '../../../../services/app.time'
+import { ToastsService } from '../../../../services/app.toasts'
+import { TranslationService } from '../../../../services/app.translation'
+import { SuperLogComponent } from '../../super-logs/super.logs.log'
 import { CaptureItem } from '../interfaces/gmp.packing.hand.washing.capture.interface'
 import { Log } from '../interfaces/gmp.packing.hand.washing.log.interface'
-
-import { DateTimeService } from '../../../../services/app.time'
-import { TranslationService } from '../../../../services/app.translation'
-import { ToastsService } from '../../../../services/app.toasts'
-import { LogService } from '../../../../services/app.logs'
-import { SuperLogComponent } from '../../super-logs/super.logs.log'
 
 @Component({
   selector: 'gmp-packing-hand-washing-log',
@@ -22,6 +20,10 @@ export class GMPPackingHandWashingLogComponent extends SuperLogComponent impleme
   @Input() log: Log = { zone_name: null, program_name: null, module_name: null, log_name: null, html_footer: null, items: [{ id: null, name: null }] }
   @Language() lang: string
 
+  readonly maxLengths = {
+    notes: 65535
+  }
+
   constructor(private _fb: FormBuilder,
     private timeService: DateTimeService,
     private translationService: TranslationService,
@@ -30,25 +32,32 @@ export class GMPPackingHandWashingLogComponent extends SuperLogComponent impleme
     super(logService, toasts)
   }
 
-  ngOnInit() {
+  public ngOnInit(): void {
     this.setSuffix("gmp-packing-hand-washing")
     super.ngOnInit()
     this.initForm()
   }
 
-  initForm() {
+  public initForm(): void {
     this.captureForm = this._fb.group({
-      date: [this.timeService.getISODate(new Date()), [Validators.required, Validators.minLength(1)]],
-      notes: ['', [Validators.required, Validators.minLength(1)]],
+      date: [this.timeService.getISODate(new Date()), [Validators.required, CustomValidators.dateValidator()]],
+      notes: ['', [Validators.required, Validators.maxLength(this.maxLengths.notes)]],
       items: this._fb.array([])
     })
-    const control = <FormArray>this.captureForm.controls['items'];
+    const control = <FormArray>this.captureForm.controls['items']
     for (let item of this.log.items) {
       control.push(this.initItem({ id: item.id, is_acceptable: false }))
     }
   }
 
-  resetForm() {
+  public initItem(item: CaptureItem): FormGroup {
+    return this._fb.group({
+      id: [item.id, [Validators.required]],
+      is_acceptable: [item.is_acceptable, [Validators.required]]
+    })
+  }
+
+  public resetForm(): void {
     let items = []
     for (let item of this.log.items) {
       items.push({ id: item.id, is_acceptable: false })
@@ -57,13 +66,6 @@ export class GMPPackingHandWashingLogComponent extends SuperLogComponent impleme
       date: this.timeService.getISODate(new Date()),
       notes: '',
       items: items
-    })
-  }
-
-  initItem(item: CaptureItem) {
-    return this._fb.group({
-      id: [item.id, [Validators.required]],
-      is_acceptable: [item.is_acceptable, [Validators.required]]
     })
   }
 }
