a4648b4215731352cda6a2f860451ff8862f21c0
VMiracle <victor.miracle@uabc.edu.mx>
2018-08-10 15:51:49 -0700

    Mejoras en la interfaz del menú

--- source/client2/dev/components/dashboard/dashboard.component.ts
+++ source/client2/dev/components/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core"
-import { DashboardFile, DashboardDirectory, AbstractDashboardElement } from "./dashboard.interface"
+import { DashboardFile, DashboardDirectory } from "./dashboard.interface"
 import { HomeElementsService } from "../../services/app.home"
 import { StateService } from "@uirouter/core"
 import { MenuService } from "../../services/app.menu"
@@ -15,6 +15,7 @@ export class DashboardComponent {
   directories: Array<string> = []
   breadcrumbs: Array<string> = []
   errorDirectory: boolean = false
+  errorNetwork: boolean = false
   loadingDirectory: boolean = true
   serverDirectory: Array<DashboardDirectory | DashboardFile> = null
 
@@ -23,139 +24,69 @@ export class DashboardComponent {
   }
 
   ngOnInit() {
-    console.log('ngOnInit', this.serverDirectory)
     if (this.serverDirectory == null) {
-      this.menuService.getMenu().then(success => {
-        this.loadingDirectory = false
-        console.log('success', success)
-        this.serverDirectory = success
-        console.log(this.serverDirectory)
-        if (this.router.params.path === '') {
-          this.currentDirectory = this.serverDirectory
-        } else {
-          this.currentDirectory = this.serverDirectory
-          let path: string = decodeURI(this.router.params.path)
-          this.directories = path.split('/')
-          //let directories: Array<string> = path.split('/')
-          //console.log(this.directories)
-          let accumulatedDirectory = ''
-          let matches = 0
-          for (let directory of this.directories) {
-            accumulatedDirectory += '/' + directory
-            this.breadcrumbs.push(accumulatedDirectory)
-            for (let icon in this.currentDirectory) {
-              //console.log('evaluated icon name: ', this.currentDirectory[icon].name)
-              if (this.currentDirectory[icon].name == directory) {
-                console.log('directory match', this.currentDirectory[icon].name, directory)
-                matches++
-                this.currentDirectory = (<DashboardDirectory>this.currentDirectory[icon]).children
-                break
-              }
-            }
-          }
-          if (matches != this.directories.length) {
-            this.currentDirectory = []
-            console.log('no te quieras pasar de verga', matches, this.directories.length)
-            this.errorDirectory = true
-          } else {
-            console.log('todo bien prro', matches, this.directories.length)
-            this.errorDirectory = false
-          }
-          console.log('directory', this.currentDirectory)
-          console.log('breadcrumbs', this.breadcrumbs)
-        }
-      }, error => {
-        console.log('error', error)
-      })
+      this.getMenu()
     } else {
-      if (this.router.params.path === '') {
-        this.currentDirectory = this.serverDirectory
-      } else {
-        this.currentDirectory = this.serverDirectory
-        let path: string = decodeURI(this.router.params.path)
-        this.directories = path.split('/')
-        //let directories: Array<string> = path.split('/')
-        //console.log(this.directories)
-        let accumulatedDirectory = ''
-        let matches = 0
-        for (let directory of this.directories) {
-          accumulatedDirectory += '/' + directory
-          this.breadcrumbs.push(accumulatedDirectory)
-          for (let icon in this.currentDirectory) {
-            //console.log('evaluated icon name: ', this.currentDirectory[icon].name)
-            if (this.currentDirectory[icon].name == directory) {
-              console.log('directory match', this.currentDirectory[icon].name, directory)
-              matches++
-              this.currentDirectory = (<DashboardDirectory>this.currentDirectory[icon]).children
-              break
-            }
-          }
-        }
-        if (matches != this.directories.length) {
-          this.currentDirectory = []
-          console.log('no te quieras pasar de verga', matches, this.directories.length)
-          this.errorDirectory = true
-        } else {
-          console.log('todo bien prro', matches, this.directories.length)
-          this.errorDirectory = false
-        }
-        console.log('directory', this.currentDirectory)
-        console.log('breadcrumbs', this.breadcrumbs)
-      }
+      this.getMenuSuccess()
     }
-    /*let temp: DashboardDirectory = { type: 'directory', name: 'BCN Packing', children: [] }
-    temp.children.push({ name: 'BCN-00 INTRO', url: '#/edit-profile', type: 'file' })
-    temp.children.push({ name: 'BCN-01 REGISTRO', url: '#/edit-profile', type: 'file' })
-    temp.children.push({ name: 'BCN-02 INFRAESTRUCTURA PREVENTIVA', url: '#/edit-profile', type: 'file' })
-    temp.children.push({ name: 'HIGIENE', url: '#/edit-profile', type: 'file' })
-
-    let temp2 = { name: 'Intro', type: 'directory', children: [] }
-
-    temp2.children.push({ image: 'https://www.w3schools.com/css/paris.jpg', name: 'Pre-operational BCN-02415-23-FG-14', url: '#/edit-profile', type: 'file' })
-    
-    temp.children.push(temp2)
-
-    //temp.children.push({ name: 'Empty Directory', type: 'directory', children: [] })
-
-    temp.children.push({ name: 'Empty Directory', type: 'directory', children: [{ name: 'Empty Directory', type: 'directory', children: [{ name: 'Empty Directory', type: 'directory', children: [{ name: 'Empty Directory', type: 'directory', children: [{ name: 'Empty Directory', type: 'directory', children: [{ name: 'Empty Directory', type: 'directory', children: [{ name: 'Empty Directory', type: 'directory', children: [{ name: 'Empty Directory', type: 'directory', children: [{ name: 'Empty Directory', type: 'directory', children: [{ name: 'Empty Directory', type: 'directory', children: [{ name: 'Empty Directory', type: 'directory', children: [{ name: 'Empty Directory', type: 'directory', children: [{ name: 'Empty Directory', type: 'directory', children: [{ name: 'Porno :V', type: 'directory', children: [] }] }] }] }] }] }] }] }] }] }] }] }] }] })
-
-    this.dashboardIcons.push(temp)
-
-    console.log('dashboard icons', this.dashboardIcons)*/
-  
-
-    //console.log(this.homeElementsService)
-    //console.debug(this.router.params.path)
-    /*this.dashboardIcons.push({ name: 'Google D', url: 'http://google.com', icon: 'google', type: 'directory' })
-    this.dashboardIcons.push({ name: 'YouTube E', url: 'http://youtube.com', icon: 'play', type: 'directory' })
-    this.dashboardIcons.push({ name: 'Facebook F', url: 'http://facebook.com', icon: 'facebook', type: 'directory' })
-    this.dashboardIcons.push({ name: 'Twitter G', url: 'http://twitter.com', icon: 'twitter', type: 'directory' })
-    this.dashboardIcons.push({ image: 'https://www.w3schools.com/css/paris.jpg', name: 'Pre-operational BCN-02415-23-FG-14', url: '#/edit-profile', type: 'file' })
-    this.dashboardIcons.push({ name: 'Hand Washing', url: '#/edit-profile', type: 'file' })
-    this.dashboardIcons.push({ name: 'Document Control', url: '#/edit-profile', type: 'file' })
-    this.dashboardIcons.push({ name: 'Finished Product', url: '#/edit-profile', type: 'file' })
-    this.dashboardIcons.push({ name: 'Scale Calibration', url: '#/edit-profile', icon: 'content-save', type: 'file' })
-    this.dashboardIcons.push({ name: 'BCN-00 INTRO', url: '#/edit-profile', type: 'file' })
-    this.dashboardIcons.push({ name: 'BCN-01 REGISTRO', url: '#/edit-profile', type: 'file' })
-    this.dashboardIcons.push({ name: 'BCN-02 INFRAESTRUCTURA PREVENTIVA', url: '#/edit-profile', type: 'file' })
-    this.dashboardIcons.push({ name: 'BCN-03 HIGIENE', url: '#/edit-profile', type: 'file' })
-    this.dashboardIcons.push({ name: 'BCN-04 ANIMALES DOMESTICOS Y SILVESTRES', url: '#/edit-profile', type: 'file' })
-    this.dashboardIcons.push({ name: 'BCN-05 CAPACITACION Y DESARROLLO DE HABILIDADES', url: '#/edit-profile', type: 'file' })
-    this.dashboardIcons.push({ name: 'BCN-06 EVALUACIONES INTERNAS Y REGULATORIAS', url: '#/edit-profile', type: 'file' })
-    this.dashboardIcons.push({ name: 'BCN-07 VALIDACIÓN', url: '#/edit-profile', type: 'file' })
-    this.dashboardIcons.push({ name: 'BCN-08 TRAZABILIDAD Y RECUPERO DE VEGETALES', url: '#/edit-profile', type: 'file' })
-    this.dashboardIcons.push({ name: 'BCN-09 HISTORIAL', url: '#/edit-profile', type: 'file' })
-    this.dashboardIcons.push({ name: 'BCN-10 AGUA DE USO AGRÍCOLA Y HUMANO', url: '#/edit-profile', type: 'file' })*/
   }
 
-  public deleteElement(event: Event, element: DashboardDirectory | DashboardFile): void {
-    event.stopPropagation()
+  public getMenu(): void {
+    this.menuService.getMenu().then(success => {
+      this.serverDirectory = success
+      this.getMenuSuccess()
+    }, error => {
+      this.getMenuError()
+    })
+  }
+
+  public getMenuSuccess(): void {
+    this.errorNetwork = false
+    this.loadingDirectory = false
+    if (this.router.params.path === '') {
+      this.currentDirectory = this.serverDirectory
+    } else {
+      this.currentDirectory = this.serverDirectory
+      this.setPath()
+      let accumulatedDirectory = ''
+      let matches = 0
+      for (let directory of this.directories) {
+        accumulatedDirectory += '/' + directory
+        this.breadcrumbs.push(accumulatedDirectory)
+        for (let icon in this.currentDirectory) {
+          if (this.currentDirectory[icon].name == directory) {
+            matches++
+            this.currentDirectory = (<DashboardDirectory>this.currentDirectory[icon]).children
+            break
+          }
+        }
+      }
+      if (matches != this.directories.length) {
+        this.currentDirectory = []
+        this.errorDirectory = true
+      } else {
+        this.errorDirectory = false
+      }
+    }
+  }
+
+  public getMenuError(): void {
+    this.setPath()
+    this.loadingDirectory = false
+    this.errorNetwork = true
+  }
+
+  public setPath(): void {
+    let path: string = decodeURI(this.router.params.path)
+    this.directories = path.split('/')
+  }
+
+  public deleteElement(element: DashboardDirectory | DashboardFile): void {
     console.log('id of element to delete: ', element.id)
     console.log('details of element to delete', element)
   }
 
-  public editElement(event: Event, element: DashboardDirectory | DashboardFile): void {
-    event.stopPropagation()
+  public editElement(element: DashboardDirectory | DashboardFile): void {
     console.log('id of element to edit: ', element.id)
     console.log('details of element to edit', element)
   }
--- source/client2/dev/services/app.menu.ts
+++ source/client2/dev/services/app.menu.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core'
 import { Observable } from 'rxjs'
 
+import { DashboardDirectory, DashboardFile } from '../components/dashboard/dashboard.interface'
 import { BackendService } from './app.backend'
 import { LoaderService } from './app.loaders'
 import { ToastsService } from './app.toasts'
@@ -13,7 +14,7 @@ export class MenuService {
 
   }
 
-  public getMenu() {
+  public getMenu(): Promise<any> {
     let menuPromise = new Promise<any>((resolve, reject) => {
       let menuLoader = this.loaderService.koiLoader()
 
@@ -21,7 +22,6 @@ export class MenuService {
         'get-menu',
         new FormData(),
         (response: any) => {
-          console.log('response side')
           if (response.meta.return_code == 0) {
             if (response.data) {
               resolve(response.data)
@@ -37,7 +37,6 @@ export class MenuService {
             this.toastService.showString("Error " + response.meta.return_code + ", server says: " + response.meta.message)
           }
         }, (error: any, caught: Observable<void>) => {
-          console.log('error side')
           reject('network error')
           menuLoader.dismiss()
           this.toastService.showText('serverUnreachable')
@@ -48,4 +47,42 @@ export class MenuService {
 
     return menuPromise
   }
+
+  public deleteElement(id: number): Promise<any> {
+    let deletePromise = new Promise<any>((resolve, reject) => {
+      let deleteLoader = this.loaderService.koiLoader()
+      let deleteForm = new FormData()
+
+      deleteForm.append('id', String(id))
+
+      this.server.update(
+        'delete-menu-element',
+        deleteForm,
+        (response: any) => {
+          if (response.meta.return_code == 0) {
+            if (response.data) {
+              resolve(response.data)
+              deleteLoader.dismiss()
+            } else {
+              reject('bad request')
+              deleteLoader.dismiss()
+              this.toastService.showText('serverUnreachable')
+            }
+          } else {
+            reject('bad request')
+            deleteLoader.dismiss()
+            this.toastService.showString("Error " + response.meta.return_code + ", server says: " + response.meta.message)
+          }
+        }, (error: any, caught: Observable<void>) => {
+          reject('network error')
+          deleteLoader.dismiss()
+          this.toastService.showText('serverUnreachable')
+          return []
+        }
+      )
+    })
+
+    return deletePromise
+  }
+
 }
