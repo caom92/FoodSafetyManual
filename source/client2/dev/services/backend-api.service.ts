692bcac98550d5b87f0fc67c8a09b8c3d13b4305
VMiracle <victor.miracle@uabc.edu.mx>
2019-03-05 15:15:45 -0800

    Archivo faltante del push anterior

---
+++ source/client2/dev/services/backend-api.service.ts
@@ -0,0 +1,116 @@
+import { Injectable } from '@angular/core'
+import { TranslationService } from 'angular-l10n'
+import { Observable } from 'rxjs'
+
+import { AlertController } from './alert/app.alert'
+import { BackendService } from './app.backend'
+import { LoaderService } from './app.loaders'
+import { ToastsService } from './app.toasts'
+import { FlattenService } from './flatten.service'
+
+@Injectable()
+export class BackendAPIService {
+  constructor(private server: BackendService,
+    private loaderService: LoaderService,
+    private flattenService: FlattenService,
+    private toastService: ToastsService,
+    private alertCtrl: AlertController,
+    private translationService: TranslationService) {
+
+  }
+
+  public service(service: string, data?: Object): Promise<any> {
+    if (service === String(service)) {
+      let promise = new Promise<any>((resolve, reject) => {
+        let loader = this.loaderService.koiLoader()
+
+        this.server.update(
+          service,
+          (data != undefined && data != null) ? this.flattenService.formDataFromFlatObject(this.flattenService.flatten(data)) : new FormData(),
+          (response: any) => {
+            this.toastService.showServerMessage(service, response.meta.return_code)
+            loader.dismiss()
+            if (response.meta.return_code == 0) {
+              resolve(response.data)
+            } else {
+              reject(response.meta.return_code)
+            }
+          }, (error: any, caught: Observable<void>) => {
+            this.toastService.showClientMessage('server-unreachable', 1)
+            loader.dismiss()
+            reject('network error')
+            return []
+          }
+        )
+      })
+
+      return promise
+    }
+
+    return null
+  }
+
+  public serviceConfirmation(service: string, titleKey: string, messageKey: string, data?: Object): Promise<any> {
+    if (service === String(service)) {
+      let promise = new Promise<any>((resolve, reject) => {
+        let alert = this.alertCtrl.create({
+          title: this.translationService.translate(titleKey),
+          message: this.translationService.translate(messageKey),
+          buttons: [
+            {
+              text: this.translationService.translate('Options.cancel'),
+              handler: () => {
+                reject('user_cancel')
+              }
+            },
+            {
+              text: this.translationService.translate('Options.accept'),
+              handler: () => {
+                this.service(service, data).then(success => {
+                  resolve(success)
+                }, error => {
+                  reject(error)
+                })
+              }
+            }
+          ]
+        })
+      })
+
+      return promise
+    }
+
+    return null
+  }
+
+
+  public testService(service: string, form?: FormData): Promise<any> {
+    if (service === String(service)) {
+      let testPromise = new Promise<any>((resolve, reject) => {
+
+        this.server.update(
+          service,
+          (form != undefined && form != null) ? form : new FormData(),
+          (response: any) => {
+            if (response.meta.return_code == 0) {
+              if (response.data) {
+                resolve(response.data)
+              } else {
+                resolve(null)
+              }
+            } else {
+              reject('bad request')
+            }
+          }, (error: any, caught: Observable<void>) => {
+            reject('network error')
+            return []
+          }
+        )
+      })
+
+      return testPromise
+    }
+
+    return null
+  }
+}
