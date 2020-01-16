868166329b4e4960014b970f527499cb8930bc2b
VMiracle <victor.miracle@uabc.edu.mx>
2020-01-16 13:37:38 -0800

    Se realizó una pequeña modificación en el reporte PDF de GAP Self Inspection Pest Control

--- source/client2/dev/components/reports/gap-self-inspection-pest-control/report/gap.self.inspection.pest.control.report.ts
+++ source/client2/dev/components/reports/gap-self-inspection-pest-control/report/gap.self.inspection.pest.control.report.ts
@@ -19,6 +19,6 @@ export class GAPSelfInspectionPestControlReportComponent extends SuperReportComp
   }
 
   public getCSS(appendCSS?: string): string {
-    return '<style>' + this.commonCSS() + ((appendCSS === String(appendCSS)) ? appendCSS : '') + '.even{background-color:#b8e0b9}.typeTitle{background-color:yellow;width:588px}.fullColumn{background-color:#D3D3D3;width:631px}.numberColumn{width:47px}.areaColumn{width:195px}.statusColumn{width:100px}.activityColumn{width:70px}.actionColumn{width:219px}</style>'
+    return '<style>' + this.commonCSS() + ((appendCSS === String(appendCSS)) ? appendCSS : '') + '.even{background-color:#b8e0b9}.typeTitle{background-color:yellow;width:588px}.fullColumn{background-color:#D3D3D3;width:631px}.numberColumn{width:47px}.areaColumn{width:195px}.statusColumn{width:100px}.activityColumn{width:70px}.actionColumn{width:266px}</style>'
   }
 }
